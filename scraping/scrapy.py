b130a306cd32699484b70b0f3f56a3bffb428afa
Denis Dymerlii <dum.denya15@gmail.com>
2024-02-09 20:19:26 +0200

    фвв get_djinni_jobs and parse_job funcs

--- scraping/scrapy.py
+++ scraping/scrapy.py
@@ -3,8 +3,9 @@ import time
 
 from bs4 import BeautifulSoup
 from httpx import AsyncClient
+from urllib.parse import urljoin
 
-HOME_URL = "https://djinni.co/jobs/"
+HOME_URL = "https://djinni.co"
 URL = "https://djinni.co/jobs/?primary_keyword=Python"
 
 
@@ -14,10 +15,33 @@ async def get_number_of_pages(client: AsyncClient) -> int:
     return int([link.text.strip() for link in soup.select("ul.pagination > li.page-item:not(.disabled) > a.page-link")][-3])
 
 
+async def get_djinni_jobs(page: int, client: AsyncClient) -> list:
+    if page == 1:
+        response = await client.get(URL)
+    else:
+        response = await client.get(URL, params={"page": page})
+    soup = BeautifulSoup(response.content, "html.parser")
+    return [job.select_one("a.job-list-item__link")["href"] for job in soup.select("li.list-jobs__item")]
+
+
+async def parse_job(link: str, client: AsyncClient):
+    response = await client.get(urljoin(HOME_URL, link))
+    soup = BeautifulSoup(response.content, "html.parser")
+    return ''.join(soup.select_one("h1").find_all(text=True, recursive=False))
+
+
 async def main():
     async with AsyncClient() as client:
         number_of_pages = await get_number_of_pages(client)
 
+        job_links = await asyncio.gather(
+            *[get_djinni_jobs(page, client) for page in range(1, number_of_pages)]
+        )
+        job_links = [job for sublist in job_links for job in sublist]
+
+        job_titles = await asyncio.gather(*[parse_job(link, client) for link in job_links])
+        for i in job_titles:
+            print(i)
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
