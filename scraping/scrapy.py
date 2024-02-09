db3c9c96dae88ffd32236dac8bd6e27d1715d5e3
Denis Dymerlii <dum.denya15@gmail.com>
2024-02-09 23:26:27 +0200

    implement all parse functionality and create csw writer

--- scraping/scrapy.py
+++ scraping/scrapy.py
@@ -1,11 +1,15 @@
 import asyncio
+import csv
 import re
 import time
+from dataclasses import astuple, fields
+from typing import NoReturn
 
 from bs4 import BeautifulSoup
 from httpx import AsyncClient
 from urllib.parse import urljoin
 from config import regex_pattern
+from scraping.models import Vacancy
 
 HOME_URL = "https://djinni.co"
 URL = "https://djinni.co/jobs/?primary_keyword=Python"
@@ -26,11 +30,35 @@ async def get_djinni_jobs(page: int, client: AsyncClient) -> list:
     return [job.select_one("a.job-list-item__link")["href"] for job in soup.select("li.list-jobs__item")]
 
 
-async def parse_job(link: str, client: AsyncClient) -> list:
+async def parse_job(link: str, client: AsyncClient) -> Vacancy:
     response = await client.get(urljoin(HOME_URL, link))
     soup = BeautifulSoup(response.content, "html.parser")
-    job_description = soup.select("div.mb-4")[0].get_text()
-    return re.findall(regex_pattern, job_description)
+    title = ''.join(soup.select_one("h1").stripped_strings)
+    company_element = soup.select_one("h4")
+    company = re.findall(r"Про компанію\s+([\w\s]+)", company_element.text.strip()) if company_element else None
+    location = "".join(soup.select_one("span.location-text").stripped_strings)
+    views_count = int(soup.select_one(".bi-eye").next_sibling.strip().split()[0])
+    reviews_count = int(soup.select_one(".bi-people-fill").next_sibling.strip().split()[0])
+    requirements = re.findall(regex_pattern, soup.select("div.mb-4")[0].get_text())
+
+    if company:
+        company = company[0]
+
+    return Vacancy(
+        title=title,
+        company=company,
+        location=location,
+        requirements=requirements,
+        views_count=views_count,
+        reviews_count=reviews_count
+    )
+
+
+def write_vacancy_to_csv(vacancies: [Vacancy]) -> NoReturn:
+    with open("vacancies.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow([field.name for field in fields(Vacancy)])
+        writer.writerows([astuple(vacancy) for vacancy in vacancies])
 
 
 async def main():
@@ -43,8 +71,8 @@ async def main():
         job_links = [job for sublist in job_links for job in sublist]
 
         job_titles = await asyncio.gather(*[parse_job(link, client) for link in job_links])
-        for i in job_titles:
-            print(i)
+        write_vacancy_to_csv(job_titles)
+
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
